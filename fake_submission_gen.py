48b5cc105b1a166b828634a7c249bfe6c26b94ab
mkisantal <kisantal.mate@gmail.com>
2019-01-08 18:01:07 +0100

    generating submission

---
+++ fake_submission_gen.py
@@ -0,0 +1,28 @@
+from submission import SubmissionWriter
+import json
+import os
+
+# load test image list
+dataset_root = '/datasets/speed_debug'
+with open(os.path.join(dataset_root, 'test.json'), 'r') as f:
+    test_set = json.load(f)
+
+submission = SubmissionWriter()
+
+
+for image in test_set[::-1]:
+
+    filename = image['filename']
+
+    # arbitrary prediction, just to store something.
+    q = [1.0, 0.0, 0.0, 0.0]
+    r = [10.0, 0.0, 0.0]
+
+    submission.append(filename, q, r)
+
+submission.export()
+print('Submission exported.')
+
+
+
+
